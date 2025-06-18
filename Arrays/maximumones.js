601dbf4c5c620e8ffb756430c40fcf1676f151f3
Sai-Revu <124420647+SaiAkhil145@users.noreply.github.com>
2025-06-19 00:42:30 +0530

    Update maximumones.js

--- Arrays/maximumones.js
+++ Arrays/maximumones.js
@@ -8,14 +8,6 @@ let maximumOnes = (arr)=>{
             count=0;
         }
     }
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]===1) {
-            count++;
-            maxCount = maxCount>count ? maxCount : count;
-        }else{
-            count=0;
-        }
-    }
     return maxCount;
 }
 console.log(maximumOnes([1,1,1,1,1,1,0,1,1,1,1]))
